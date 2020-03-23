8cc06d6abdb9809244f02fa92a5cf701d93bf311
pandaCure <zzyamor@163.com>
2020-03-23 16:11:10 +0800

    初始化项目

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,58 @@
+module.exports = {
+  title: '钟繇',
+  description: 'JavaScript知识点',
+  head: [
+    ['link', { rel: 'icon', href: `/logo.png` }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    [
+      'meta',
+      { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }
+    ],
+    [
+      'link',
+      { rel: 'apple-touch-icon', href: `/icons/apple-touch-icon-152x152.png` }
+    ],
+    [
+      'link',
+      {
+        rel: 'mask-icon',
+        href: '/icons/safari-pinned-tab.svg',
+        color: '#3eaf7c'
+      }
+    ],
+    [
+      'meta',
+      {
+        name: 'msapplication-TileImage',
+        content: '/icons/msapplication-icon-144x144.png'
+      }
+    ],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
+  ], // 配置.ico,
+  configureWebpack: {}, // webpack配置
+  themeConfig: {
+    sidebar: {
+      '/topics/': [
+        {
+          title: '知识点',
+          collapsable: false,
+          children: ['']
+        }
+      ],
+      '/': [
+        {
+          title: '主页',
+          collapsable: false,
+          children: ['']
+        }
+      ]
+    }, // 侧边栏
+    nav: [
+      { text: '主页', link: '/' },
+      { text: '知识点', link: '/topics/' }
+    ] // 导航
+  },
+  base: '/topics/'
+}
